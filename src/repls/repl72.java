c525e320908f9cf365db8616d34c93f19c06b34e
darynazarenko <darynazarenko95@gmail.com>
2021-03-10 21:41:44 -0600

    final commit

---
+++ src/repls/repl72.java
@@ -0,0 +1,17 @@
+package repls;
+
+public class repl72 {
+
+	
+		public static void main(String[] args) {
+			int m=2, total=0;
+
+			while(m<6) {
+
+			   total=total+m;
+
+			   m++;
+
+			}
+
+			System.out.print(total);}}
